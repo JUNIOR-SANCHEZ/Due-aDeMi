852255b35d4ab8dd3c33dcf2832b98902fee149c
RolandoRomero25 <rolandoromeroh@outlook.com>
2018-09-22 11:28:24 -0500

    formulario PGF

--- modules/tutoras/controllers/pgfController.php
+++ modules/tutoras/controllers/pgfController.php
@@ -1,1 +1,10 @@
 <?php
+class pgfController extends tutorasController
+{
+    public function __construct(){
+        parent::__construct();
+    }
+    public function index(){
+        $this->_view->renderizar("nuevo");
+    }
+}
